358f5781867b4425139c164dd9956c99b5171cc7
salehbagomri <s.bagomri@gmail.com>
2026-06-09 14:45:58 +0300

    ui: hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'الفواتير',
       theme: getAppTheme(),
       locale: const Locale('ar'),
